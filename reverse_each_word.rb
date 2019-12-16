55c57af20ab3757c8a8d9da7dafb23defcda2760
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-16 15:19:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word (source_array)
   new_array =[]
   i = 0 
   while i<source_array[i].lenght do
-  new_array.split (' ')
+  new_array= source_array.split (" ")
   i +=1 
   return 
   new_array
