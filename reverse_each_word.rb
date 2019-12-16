534c23b3e80b679194db9c192bc9ae80b531deb0
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-16 15:52:12 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word (string)
+  original_array = string.split(" ")
+  new_array =[]
+  original_array.each do |string|
+    new_array << string.reverse
+end
+new_array.join (" ")
+end
+
+def reverse_each_word (string)
+  array = string.split(" ")
+  new_array =[]
+  array.collect do |string|
+    new_array << string.reverse
+end
+new_array.join (" ")
+end
+
+  
+  
+  
