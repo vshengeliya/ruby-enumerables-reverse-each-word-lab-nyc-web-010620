b47c6211c0ead0d14f02d8089eddf5e5c508c94f
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-16 15:17:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (source_array)
   new_array =[]
   i = 0 
-  while <source_array[i].lenght do
+  while i<source_array[i].lenght do
   new_array.split (' ')
   i +=1 
   return 
